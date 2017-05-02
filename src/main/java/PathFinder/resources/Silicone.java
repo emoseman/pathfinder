b39f52ecf602593fc6a9e86918886f4e324fc962
Santerz <santerzgithub@gmail.com>
2017-05-02 16:13:34 -0400

    Create Silicone.java

---
+++ src/main/java/PathFinder/resources/Silicone.java
@@ -0,0 +1,18 @@
+package PathFinder.resources;
+
+import PathFinder.model.BaseResource;
+
+/**
+ * Lithium
+ *
+ * @author Evan Moseman (evan.moseman@corp.aol.com)
+ * @version 1.0
+ * @since 4/20/17
+ */
+public class Silicone
+    extends BaseResource {
+
+    public Silicone() {
+        super("Silicone", "For some reason all I want to build is a cone right now.", 2.0F, new byte[0]);
+    }
+}
