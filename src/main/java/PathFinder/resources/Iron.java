9f9d02750b013b13e1977abb9a242d6145ab3d6d
Santerz <santerzgithub@gmail.com>
2017-05-02 16:00:47 -0400

    Create Iron.java

---
+++ src/main/java/PathFinder/resources/Iron.java
@@ -0,0 +1,18 @@
+package PathFinder.resources;
+
+import PathFinder.model.BaseResource;
+
+/**
+ * Lithium
+ *
+ * @author Evan Moseman (evan.moseman@corp.aol.com)
+ * @version 1.0
+ * @since 4/20/17
+ */
+public class Iron
+    extends BaseResource {
+
+    public Iron() {
+        super("Iron", "A very heavy piece of metal.", 10.0F, new byte[0]);
+    }
+}
