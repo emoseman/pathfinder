9f91cf3485ee57dff9e06de85c6436563be5bb2a
Santerz <santerzgithub@gmail.com>
2017-05-02 16:08:35 -0400

    Create Rubber.java

---
+++ src/main/java/PathFinder/resources/Rubber.java
@@ -0,0 +1,18 @@
+package PathFinder.resources;
+
+import PathFinder.model.BaseResource;
+
+/**
+ * Lithium
+ *
+ * @author Evan Moseman (evan.moseman@corp.aol.com)
+ * @version 1.0
+ * @since 4/20/17
+ */
+public class Rubber
+    extends BaseResource {
+
+    public Rubber() {
+        super("Rubber", "A bouncy material that is very intertaining to see bounce everywhere.", 3.0F, new byte[0]);
+    }
+}
