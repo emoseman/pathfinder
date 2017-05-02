50c1d9a7abc9e8e8a66605cc5c6d177d6a516b7e
Santerz <santerzgithub@gmail.com>
2017-05-02 15:40:40 -0400

    Lead.java

---
+++ src/main/java/PathFinder/resources/Lead.java
@@ -0,0 +1,18 @@
+package PathFinder.resources;
+
+import PathFinder.model.BaseResource;
+
+/**
+ * Lithium
+ *
+ * @author Evan Moseman (evan.moseman@corp.aol.com)
+ * @version 1.0
+ * @since 4/20/17
+ */
+public class Lead
+    extends BaseResource {
+
+    public Lead() {
+        super("Lead", "A soft black metal. ", 20F, new byte[0]);
+    }
+}
