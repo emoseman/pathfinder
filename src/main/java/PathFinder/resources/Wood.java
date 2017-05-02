64e65cd6850a7c30677ea436b4ec56e01bfbeb00
Santerz <santerzgithub@gmail.com>
2017-05-02 15:45:00 -0400

    Wood.java

---
+++ src/main/java/PathFinder/resources/Wood.java
@@ -0,0 +1,18 @@
+package PathFinder.resources;
+
+import PathFinder.model.BaseResource;
+
+/**
+ * Lithium
+ *
+ * @author Evan Moseman (evan.moseman@corp.aol.com)
+ * @version 1.0
+ * @since 4/20/17
+ */
+public class Wood
+    extends BaseResource {
+
+    public Wood() {
+        super("Wood", "An organic resource that comes from trees. ", 5.5F, new byte[0]);
+    }
+}
