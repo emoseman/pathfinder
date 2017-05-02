58d343b49514b1ecd90908194469947beb669e59
Santerz <santerzgithub@gmail.com>
2017-05-02 15:50:15 -0400

    Create Carbon.java

---
+++ src/main/java/PathFinder/resources/Carbon.java
@@ -0,0 +1,18 @@
+package PathFinder.resources;
+
+import PathFinder.model.BaseResource;
+
+/**
+ * Lithium
+ *
+ * @author Evan Moseman (evan.moseman@corp.aol.com)
+ * @version 1.0
+ * @since 4/20/17
+ */
+public class Carbon
+    extends BaseResource {
+
+    public Carbon() {
+        super("Carbon", "A dull grey substance, wonder what it's used for? ", 6.0F, new byte[0]);
+    }
+}
