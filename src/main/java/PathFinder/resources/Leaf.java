ddd279616ed6a1953977545f83841c088770546d
Santerz <santerzgithub@gmail.com>
2017-05-02 15:48:10 -0400

    Create Leaf.java

---
+++ src/main/java/PathFinder/resources/Leaf.java
@@ -0,0 +1,18 @@
+package PathFinder.resources;
+
+import PathFinder.model.BaseResource;
+
+/**
+ * Lithium
+ *
+ * @author Evan Moseman (evan.moseman@corp.aol.com)
+ * @version 1.0
+ * @since 4/20/17
+ */
+public class Leaf
+    extends BaseResource {
+
+    public Leaf() {
+        super("Leaf", "Some leaves you found in the forest. ", 0.05F, new byte[0]);
+    }
+}
