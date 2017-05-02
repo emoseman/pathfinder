bdb73d50c8cf0489d6031d64f838d8eac2635201
Evan <evan.moseman@gmail.com>
2017-05-02 17:24:57 -0400

    Create Lead.java

---
+++ src/main/java/PathFinder/resources/Lead.java
@@ -0,0 +1,11 @@
+package PathFinder.resources;
+
+import PathFinder.model.BaseResource;
+
+public class Lead
+    extends BaseResource {
+
+    public Lead() {
+        super("Lead", "A very heavy dark metal used in other recipes", 3.0F, new byte[0]);
+    }
+}
