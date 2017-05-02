d229222f7fe51cea29314fb3a0d958605d07ce63
Santerz <santerzgithub@gmail.com>
2017-05-02 16:06:13 -0400

    Create Sap.java

---
+++ src/main/java/PathFinder/resources/Sap.java
@@ -0,0 +1,18 @@
+package PathFinder.resources;
+
+import PathFinder.model.BaseResource;
+
+/**
+ * Lithium
+ *
+ * @author Evan Moseman (evan.moseman@corp.aol.com)
+ * @version 1.0
+ * @since 4/20/17
+ */
+public class Sap
+    extends BaseResource {
+
+    public Sap() {
+        super("Sap", "An increadibly sticky substance that I've been picking off of everything for hours!", 0.01F, new byte[0]);
+    }
+}
