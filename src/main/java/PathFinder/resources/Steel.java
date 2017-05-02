a8415d2050639f4abe2a8f631350e7dd085a7b43
Santerz <santerzgithub@gmail.com>
2017-05-02 15:59:25 -0400

    Create Steel.java

---
+++ src/main/java/PathFinder/resources/Steel.java
@@ -0,0 +1,18 @@
+package PathFinder.resources;
+
+import PathFinder.model.BaseResource;
+
+/**
+ * Lithium
+ *
+ * @author Evan Moseman (evan.moseman@corp.aol.com)
+ * @version 1.0
+ * @since 4/20/17
+ */
+public class Steel
+    extends BaseResource {
+
+    public Steel() {
+        super("Steel", "*Insert steel pun here*", 15.0F, new byte[0]);
+    }
+}
