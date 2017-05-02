c633f5ea7d6a4066b138d82d8e27be2f34b1acca
Santerz <santerzgithub@gmail.com>
2017-05-02 15:43:54 -0400

    Plastic.java

---
+++ src/main/java/PathFinder/resources/Plastic.java
@@ -0,0 +1,18 @@
+package PathFinder.resources;
+
+import PathFinder.model.BaseResource;
+
+/**
+ * Lithium
+ *
+ * @author Evan Moseman (evan.moseman@corp.aol.com)
+ * @version 1.0
+ * @since 4/20/17
+ */
+public class Plastic
+    extends BaseResource {
+
+    public Lithium() {
+        super("Plastic", "A very light and cloudy resource. ", 1.0F, new byte[0]);
+    }
+}
