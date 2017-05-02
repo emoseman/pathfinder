5ad81a05044cb607122354490eba07b970aa9bc6
Santerz <santerzgithub@gmail.com>
2017-05-02 15:47:03 -0400

    Create Paint.java

---
+++ src/main/java/PathFinder/resources/Paint.java
@@ -0,0 +1,18 @@
+package PathFinder.resources;
+
+import PathFinder.model.BaseResource;
+
+/**
+ * Lithium
+ *
+ * @author Evan Moseman (evan.moseman@corp.aol.com)
+ * @version 1.0
+ * @since 4/20/17
+ */
+public class Paint
+    extends BaseResource {
+
+    public Paint() {
+        super("Paint", "A bucket of custom paint.  How interesting. ", 0.001F, new byte[0]);
+    }
+}
